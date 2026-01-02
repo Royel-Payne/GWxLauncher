973af12f12a043791c5de3c28c1bb351066967de
Royel-Payne <mmoroyel@gmail.com>
2026-01-02 14:23:57 -0800

    Refine dark and light theme color palettes for clarity
    Updated both DarkPalette and LightPalette to improve visual clarity, contrast, and accessibility. Adjusted backgrounds, foregrounds, borders, and accent colors for better distinction between UI elements and a more modern, cohesive appearance in both dark and light modes. These changes enhance readability and overall user experience.

--- UI/DarkPalette.cs
+++ UI/DarkPalette.cs
@@ -4,53 +4,53 @@ namespace GWxLauncher.UI
 {
     internal sealed class DarkPalette : ThemePalette
     {
-        public override Color WindowBack => Color.FromArgb(24, 24, 28);
-        public override Color WindowFore => Color.Gainsboro;
+        // Darker background creates depth behind the cards
+        public override Color WindowBack => Color.FromArgb(18, 18, 20);
+        public override Color WindowFore => Color.FromArgb(220, 220, 225);
 
-        public override Color SurfaceBack => Color.FromArgb(30, 30, 36);
-        public override Color SurfaceBorder => Color.FromArgb(60, 60, 70);
+        public override Color SurfaceBack => Color.FromArgb(28, 28, 32);
+        public override Color SurfaceBorder => Color.FromArgb(45, 45, 50);
 
-        public override Color HeaderBack => Color.FromArgb(30, 30, 36);
-        public override Color Separator => Color.FromArgb(60, 60, 70);
+        public override Color HeaderBack => Color.FromArgb(28, 28, 32);
+        public override Color Separator => Color.FromArgb(40, 40, 45);
 
-        public override Color ButtonBack => Color.FromArgb(45, 45, 52);
-        public override Color ButtonBorder => Color.FromArgb(80, 80, 90);
+        public override Color ButtonBack => Color.FromArgb(45, 45, 55);
+        public override Color ButtonBorder => Color.FromArgb(70, 70, 85);
         public override Color ButtonFore => Color.White;
 
         public override Color PrimaryButtonBack => Color.FromArgb(0, 120, 215);
         public override Color PrimaryButtonBorder => Color.FromArgb(0, 100, 180);
         public override Color PrimaryButtonFore => Color.White;
 
-        public override Color DangerButtonBack => Color.FromArgb(180, 60, 60);
-        public override Color DangerButtonBorder => Color.FromArgb(150, 45, 45);
+        public override Color DangerButtonBack => Color.FromArgb(200, 50, 50);
+        public override Color DangerButtonBorder => Color.FromArgb(170, 40, 40);
         public override Color DangerButtonFore => Color.White;
 
-        public override Color InputBack => Color.FromArgb(18, 18, 22);
-        public override Color InputFore => Color.Gainsboro;
-        public override Color InputBorder => Color.FromArgb(80, 80, 90);
+        public override Color InputBack => Color.FromArgb(12, 12, 15);
+        public override Color InputFore => Color.White;
+        public override Color InputBorder => Color.FromArgb(60, 60, 75);
 
-        public override Color SubtleFore => Color.FromArgb(180, 180, 190);
-        public override Color DisabledFore => Color.FromArgb(140, 140, 150);
+        public override Color SubtleFore => Color.FromArgb(150, 150, 165);
+        public override Color DisabledFore => Color.FromArgb(90, 90, 100);
 
-        public override Color CardBack => Color.FromArgb(35, 35, 40);
-        public override Color CardSelectedBack => Color.FromArgb(45, 45, 52);
+        // Lighter cards on darker window = High Definition
+        public override Color CardBack => Color.FromArgb(32, 32, 38);
+        public override Color CardSelectedBack => Color.FromArgb(40, 40, 50);
 
-        public override Color CardBorder => Color.FromArgb(70, 70, 80);
+        public override Color CardBorder => Color.FromArgb(55, 55, 65);
         public override Color CardSelectedBorder => Color.FromArgb(0, 120, 215);
 
         public override Color CardNameFore => Color.White;
-        public override Color CardSubFore => Color.Silver;
+        public override Color CardSubFore => Color.FromArgb(170, 170, 185);
 
-        public override Color BadgeBack => Color.FromArgb(60, 60, 70);
-        public override Color BadgeBorder => Color.FromArgb(90, 90, 110);
-        public override Color BadgeFore => Color.Gainsboro;
+        public override Color BadgeBack => Color.FromArgb(50, 50, 65);
+        public override Color BadgeBorder => Color.FromArgb(75, 75, 95);
+        public override Color BadgeFore => Color.FromArgb(210, 210, 220);
 
-        public override Color HoverBack => Color.FromArgb(40, 40, 46);
-        public override Color HoverBorder => Color.FromArgb(95, 95, 110);
+        public override Color HoverBack => Color.FromArgb(42, 42, 52);
+        public override Color HoverBorder => Color.FromArgb(85, 85, 110);
 
         public override Color Accent => Color.FromArgb(0, 120, 215);
-
-        public override Color AccentHover => Color.FromArgb(0, 120, 215, 140);
-
+        public override Color AccentHover => Color.FromArgb(0, 120, 215, 150);
     }
 }
--- UI/LightPalette.cs
+++ UI/LightPalette.cs
@@ -4,53 +4,52 @@ namespace GWxLauncher.UI
 {
     internal sealed class LightPalette : ThemePalette
     {
-        public override Color WindowBack => Color.FromArgb(245, 245, 248);
-        public override Color WindowFore => Color.FromArgb(28, 28, 32);
+        public override Color WindowBack => Color.FromArgb(235, 235, 240);
+        public override Color WindowFore => Color.FromArgb(30, 30, 35);
 
-        public override Color SurfaceBack => Color.FromArgb(236, 236, 240);
-        public override Color SurfaceBorder => Color.FromArgb(200, 200, 210);
+        public override Color SurfaceBack => Color.FromArgb(245, 245, 250);
+        public override Color SurfaceBorder => Color.FromArgb(200, 200, 215);
 
-        public override Color HeaderBack => SurfaceBack;
-        public override Color Separator => SurfaceBorder;
+        public override Color HeaderBack => Color.FromArgb(245, 245, 250);
+        public override Color Separator => Color.FromArgb(190, 190, 205);
 
-        public override Color ButtonBack => Color.FromArgb(230, 230, 236);
-        public override Color ButtonBorder => Color.FromArgb(190, 190, 200);
-        public override Color ButtonFore => Color.FromArgb(28, 28, 32);
+        public override Color ButtonBack => Color.FromArgb(225, 225, 235);
+        public override Color ButtonBorder => Color.FromArgb(180, 180, 200);
+        public override Color ButtonFore => Color.FromArgb(45, 45, 55);
 
-        public override Color PrimaryButtonBack => Color.FromArgb(0, 120, 215);
-        public override Color PrimaryButtonBorder => Color.FromArgb(0, 100, 180);
+        public override Color PrimaryButtonBack => Color.FromArgb(0, 105, 210);
+        public override Color PrimaryButtonBorder => Color.FromArgb(0, 85, 180);
         public override Color PrimaryButtonFore => Color.White;
 
-        public override Color DangerButtonBack => Color.FromArgb(200, 60, 60);
-        public override Color DangerButtonBorder => Color.FromArgb(170, 45, 45);
+        public override Color DangerButtonBack => Color.FromArgb(210, 45, 45);
+        public override Color DangerButtonBorder => Color.FromArgb(180, 35, 35);
         public override Color DangerButtonFore => Color.White;
 
         public override Color InputBack => Color.White;
-        public override Color InputFore => Color.FromArgb(28, 28, 32);
-        public override Color InputBorder => Color.FromArgb(190, 190, 200);
+        public override Color InputFore => Color.Black;
+        public override Color InputBorder => Color.FromArgb(170, 170, 190);
 
-        public override Color SubtleFore => Color.FromArgb(90, 90, 100);
-        public override Color DisabledFore => Color.FromArgb(150, 150, 160);
+        public override Color SubtleFore => Color.FromArgb(90, 95, 115);
+        public override Color DisabledFore => Color.FromArgb(160, 160, 175);
 
-        public override Color CardBack => SurfaceBack;
-        public override Color CardSelectedBack => Color.FromArgb(225, 225, 232);
+        // White cards on soft gray window = High Contrast
+        public override Color CardBack => Color.White;
+        public override Color CardSelectedBack => Color.FromArgb(235, 242, 255);
 
-        public override Color CardBorder => SurfaceBorder;
-        public override Color CardSelectedBorder => Color.FromArgb(0, 120, 215);
+        public override Color CardBorder => Color.FromArgb(185, 185, 205);
+        public override Color CardSelectedBorder => Color.FromArgb(0, 105, 210);
 
-        public override Color CardNameFore => WindowFore;
-        public override Color CardSubFore => Color.FromArgb(80, 80, 90);
+        public override Color CardNameFore => Color.Black;
+        public override Color CardSubFore => Color.FromArgb(85, 85, 105);
 
-        public override Color BadgeBack => Color.FromArgb(220, 220, 228);
-        public override Color BadgeBorder => SurfaceBorder;
-        public override Color BadgeFore => Color.FromArgb(70, 70, 80);
+        public override Color BadgeBack => Color.FromArgb(230, 235, 245);
+        public override Color BadgeBorder => Color.FromArgb(200, 205, 220);
+        public override Color BadgeFore => Color.FromArgb(60, 65, 85);
 
-        public override Color HoverBack => Color.FromArgb(232, 232, 238);
-        public override Color HoverBorder => Color.FromArgb(180, 180, 190);
+        public override Color HoverBack => Color.FromArgb(248, 250, 255);
+        public override Color HoverBorder => Color.FromArgb(160, 175, 210);
 
-        public override Color Accent => Color.FromArgb(0, 120, 215);
-
-        public override Color AccentHover => Color.FromArgb(0, 120, 215, 120);
-
+        public override Color Accent => Color.FromArgb(0, 105, 210);
+        public override Color AccentHover => Color.FromArgb(0, 105, 210, 120);
     }
 }
